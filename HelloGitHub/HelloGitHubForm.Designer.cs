c6746a4b9aaa735d220d243ed2d0a8346b718f23
Jonathan Levesque <ejlevesq31@monccnb.ca>
2021-09-24 13:41:42 -0300

    Add project files.

---
+++ HelloGitHub/HelloGitHubForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace HelloGitHub
+{
+    partial class HelloGitHubForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HelloGitHubForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 341);
+            this.Name = "HelloGitHubForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello GitHub";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
